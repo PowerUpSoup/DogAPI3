0f0babc5756b2d67917ae64f7d26f5c5d30fb0c4
Sean Jackson <PowerUpSoup@gmail.com>
2019-08-24 18:01:03 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ function getDogImage() {
     console.log(BreedofDog);
     fetch('https://dog.ceo/api/breed/' + BreedofDog + '/images/random')
         .then(response => response.json())
-        .then(responseJson => status)
         .then(responseJson => displayResults(responseJson))
-        .catch(error => alert('Something went wrong. Check your spelling and your internet connection!'));
+        .catch(error => alert('Something went wrong. Check your internet connection!'));
 }
 
 function displayResults(responseJson) {
     //clear any previous results
     $('.results-img').remove();
-    //write the results to the console
+    //write the API response to the console
     console.log(responseJson);
-    //add an element with the image fetched frem the API
-    $('div').append(`<img src="${responseJson.message}" class="results-img">`);
-    //display the results section on the first use
-    $('section').removeClass('hidden');
+
+    //set up a loop that 
+    if (responseJson.status == "success") {
+        console.log("it was a success")
+        //add an element with the image fetched frem the API
+        $('div').append(`<img src="${responseJson.message}" class="results-img">`);
+        //display the results section on the first use
+        $('section').removeClass('hidden');
+    }
+    if (responseJson.status == "error") {
+        console.log("it was an error")
+        alert('Something went wrong. Check your spelling!');
+    }
 }
 
 function watchForm() {
